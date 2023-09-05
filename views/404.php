9c38d2746ee1bf6bb8dc6ea722295b868670f415
Naio200000 <90395089+Naio200000@users.noreply.github.com>
2023-09-05 17:32:09 -0300

    se agrega pagina de error

---
+++ views/404.php
@@ -0,0 +1,5 @@
+<div class="carousel-inner">      
+    <div class='carousel-item active' data-bs-interval='10000'>
+        <img src='./img/hero/error/samurai-perdido.jpg' class='d-block w-100' alt='Grupo de estdiantes de Iaido'>
+    </div>
+</div>
