effb728178d8ad547cff3e74d84490b721cce7c6
Naio200000 <90395089+Naio200000@users.noreply.github.com>
2023-10-29 20:07:35 -0300

    feat: Creat Conexion Class

---
+++ classes/Conexion.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Clase para generar nuestras conexiones a la BD
+ */
+class Conexion {
+    private const DB_HOST = 'localhost';
+    private const DB_USER = 'root';
+    private const DB_PASS = '';
+    private const DB_NAME = 'pii_komei';
+    private const DB_DSN = 'mysql:host=' . self::DB_HOST . ';dbname=' . self::DB_NAME . ';charset=utf8mb4';
+
+    private PDO $db;
+
+
+    public function __construct() {
+        try {
+            $this->db = new PDO(self::DB_DSN, self::DB_USER, self::DB_PASS);
+        } catch (Exception $e) {
+            die('Error al conectar con la Base de datos.');
+        }
+    }
+
+    /**
+     * Devuelve una conexión PDO
+     * @return PDO
+     */
+    public function getConexion(): PDO {
+        return $this->db;
+    }
+}
