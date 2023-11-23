bf2077e283776b4281c1159a7961e249d8e2db72
Naio200000 <90395089+Naio200000@users.noreply.github.com>
2023-11-23 00:11:57 -0300

    feat: add alert to tipo abm

--- admin/acciones/abm-tipo-accion.php
+++ admin/acciones/abm-tipo-accion.php
@@ -9,21 +9,27 @@
         if (!$id) {
             $id_tipo = $tipo->insertTipo($datosPOST);
             $tipo->insertTipoXCategoria($id_tipo, $datosPOST['id_categoria']);
+            (new Alert())->insertAlerta('success', "Se agrego un tipo correctamente");
         } else {
             if (!$del) {
                 $tipo->editTipo($datosPOST);
                 $tipo->editTipoXCategoria($datosPOST['id_categoria']);
-
+                (new Alert())->insertAlerta('success', "Se edito el tipo {$tipo->getName()} correctamente");                
             } else {
                 $tipo->deleteTipo();
+                (new Alert())->insertAlerta('danger', "Se borro la el tipo {$tipo->getName()}");
             }
         }
 
         header('Location: ../index.php?view=tipo');
 
     } catch (Exception $e) {
-        echo '<pre>';
-        print_r($e);
-        echo '</pre>';
-        die('No se pudo cargar la Categoria');
+        // echo '<pre>';
+        // print_r($e);
+        // echo '</pre>';
+        // die('No se pudo cargar la Categoria');
+
+
+        (new Alert())->insertAlerta('danger', "Hubo un error inesperado. Comunicarse con el Administrador de sistema.");
+        header('Location: ../index.php?view=categoria');
     }
