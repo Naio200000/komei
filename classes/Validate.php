4b49efa8f83380a1ee3ed4abe959c4dbaa27da5c
Nicolas Alsinet <nicolas.alsinet@gmail.com>
2023-11-23 12:01:03 -0300

    feat add delete and get form in Validate

--- classes/Validate.php
+++ classes/Validate.php
@@ -4,10 +4,41 @@
 class Validate {
 
 
-
+    /**
+     * inserta los datos del form en la session
+     * @param array $datosForm los datos que vienen del formulario
+     */
     public function inserForm (array $datosForm) {
 
         $_SESSION['form'] = $datosForm;
     }
-    
+        
+    /**
+     * Borra las alertas de las $_SESSION
+     */
+    public function deleteForm() {
+
+        $_SESSION['form'] = [];
+    }
+
+        /**
+     * Devuelve los datos de form de la variable session y los borra
+     * @return ?array devuelve un array con los datos del form
+     */
+    public function getForm() :?array {
+
+        if (!empty($_SESSION['form'])) {
+
+            $form = $_SESSION['form'];
+
+            $this->deleteForm();
+            return $form;
+
+        } else {
+
+            return null;
+        }
+    }
+
+
 }
