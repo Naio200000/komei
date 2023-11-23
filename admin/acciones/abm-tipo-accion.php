df3f531c72ad4c6d0218fcd0f61fe78c6d061e73
Naio200000 <90395089+Naio200000@users.noreply.github.com>
2023-11-23 00:48:37 -0300

    mino change

--- admin/acciones/abm-tipo-accion.php
+++ admin/acciones/abm-tipo-accion.php
@@ -28,8 +28,10 @@
         // print_r($e);
         // echo '</pre>';
         // die('No se pudo cargar la Categoria');
-
-
-        (new Alert())->insertAlerta('danger', "Hubo un error inesperado. Comunicarse con el Administrador de sistema.");
-        header('Location: ../index.php?view=categoria');
+        if ($e->getCode() == 23000) {
+            (new Alert())->insertAlerta('danger', "El tipo {$tipo->getName()} no se pudo eliminaro porque esta asociada con un o mas Productos o a una Categoria.");
+        } else {
+            (new Alert())->insertAlerta('danger', "Hubo un error inesperado. Comunicarse con el Administrador de sistema.");
+        }
+        header('Location: ../index.php?view=tipo');
     }
