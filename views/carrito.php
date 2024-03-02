d37301c239f093c5ef4e137153a9ec8593ef3009
Naio200000 <90395089+Naio200000@users.noreply.github.com>
2024-03-02 00:07:55 -0300

    feat; add carrito template

--- views/carrito.php
+++ views/carrito.php
@@ -1,57 +1,28 @@
 <section class="item container-fluid container-md pb-3">
-    <?PHP
-    if (!empty($item)) {?>
-        <h2 class="titulo-seccion w-75 w-lg-100 text-uppercase text-center fw-bold my-2 mx-auto px-2"><?= $item->getNombre()?></h2>
-        <p class="fs-5 w-75 mx-auto fw-bold text-center"><?= $item->formatearDescript()?></p>
-        <div class="card">
-            <div class="row">
-                <div class="col-lg-6 d-flex flex-column">
-                    <div id="carouselProductoImagen" class="carousel slide mx-auto" data-bs-ride="true">
-                        <div class="carousel-indicators">
-                            <button type="button" data-bs-target="#carouselProductoImagen" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                            <button type="button" data-bs-target="#carouselProductoImagen" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                            <button type="button" data-bs-target="#carouselProductoImagen" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                        </div>
-                        <div class="carousel-inner p-5" id="carrusel-producto">
-                            <?php
-                                $i = 0;
-                                foreach ($item->getImagen() as $v) { ?>
-                                <div class="carousel-item <?php if ($i == 0 ) echo 'active'; ?>">
-                                    <img class="card-img-top" src="./img/productos/<?= $v->getName() ?>.webp" alt="<?= $v->getDescript() ?>">
-                                </div>
-                            <?php $i++; }?>
-                        </div>
-                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselProductoImagen" data-bs-slide="prev">
-                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span class="visually-hidden">Anterior</span>
-                        </button>
-                        <button class="carousel-control-next" type="button" data-bs-target="#carouselProductoImagen" data-bs-slide="next">
-                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span class="visually-hidden">Proximo</span>
-                        </button>
-                    </div>
-                    <span class="text-uppercase p-2 text-lg-start text-end"><?= $item->getCategoria()->getName()?></span>
-                </div>
-                <div class="col-lg-6 d-flex flex-column p-3">
-                    <div class="card-body flex-grow-0">
-                        <h3 class="card-title fs-2"><?= $item->getNombre()?></h3>
-                        <p class="card-text fs-5"><?= $item->formatearDescript(true)?></p>
-                    </div>
-                    <ul class="list-group list-group-flush">
-                        <li class='list-group-item fs-5'><span class='fw-bold text-capitalize'> Disponibilidad:  </span><?= $item->formatearFecha() ?></li>
-                        <li class='list-group-item fs-5'><span class='fw-bold text-capitalize'> Tipo:  </span><?= $item->getTipo()->getName() ?></li>
-                        <?php foreach ($item->formatearCaravalOBJ() as $k => $v) { ?>
-                            <li class='list-group-item fs-5 text-capitalize'><span class='fw-bold text-capitalize'> <?= "$k : </span> $v </li>"?>
-                            <?php }?>
-                    </ul>
-                    <div class="card-body flex-grow-0 mt-auto" id="botonProducto">
-                        <p class="fs-3 mb-3 fw-bold text-center preciocard"><?= $item->formatearPrecio()?></p>
-                        <button id="" class="btn btn-komei w-100 fs-3 text-uppercase fw-bold">Comprar</button>
-                </div>
+    <h2 class="titulo-seccion w-75 w-lg-100 text-uppercase text-center fw-bold my-2 mx-auto px-2">Tu Carrito</h2>
+    <div class="contenedorProductosCarrito">
+        <div class="contenedor rounded-2">
+            <div class="productoCarritoImagen mx-auto">
+                <img class="rounded-2" src="./img/productos/1700660093.webp" alt="">
+            </div>
+            <div class="productoCarritoTexto my-auto">
+                <p class="h4">Titulo del producto</p>
+                <p>Descripcion del producot</p>
+            </div>
+            <div class="productoCarritoCerrar ms-auto d-flex puntero">
+                <span>
+                    <span class="me-1"></span>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+                        <path id="${i}pathProducto" d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
+                    </svg>
+                </span>
+            </div>
+            <div class="productoCarritoPrecio ms-auto">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-currency-dollar" viewBox="0 0 16 16"><path d="M4 10.781c.148 1.667 1.513 2.85 3.591 3.003V15h1.043v-1.216c2.27-.179 3.678-1.438 3.678-3.3 0-1.59-.947-2.51-2.956-3.028l-.722-.187V3.467c1.122.11 1.879.714 2.07 1.616h1.47c-.166-1.6-1.54-2.748-3.54-2.875V1H7.591v1.233c-1.939.23-3.27 1.472-3.27 3.156 0 1.454.966 2.483 2.661 2.917l.61.162v4.031c-1.149-.17-1.94-.8-2.131-1.718H4zm3.391-3.836c-1.043-.263-1.6-.825-1.6-1.616 0-.944.704-1.641 1.8-1.828v3.495l-.2-.05zm1.591 1.872c1.287.323 1.852.859 1.852 1.769 0 1.097-.826 1.828-2.2 1.939V8.73l.348.086z"/></svg><span>2000.00</span>
             </div>
         </div>
-        <?PHP } else {?>
-            <h2 class="titulo-seccion w-75 w-lg-100 text-uppercase text-center fw-bold my-2 mx-auto px-2">No pudimos encontrar el producto que buscabas.</h2>
-            <p class="fs-5 w-75 mx-auto fw-bold text-center fs-5">Para volver a la home hás click <a class="fs-3" href="index.php?view=home">aquí.</a></p>
-        <?PHP }?>
+        <div class="p-3 fs-5">
+            <p class="my-3 text-end">Total <span class="fw-bold"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-currency-dollar" viewBox="0 0 16 16"><path d="M4 10.781c.148 1.667 1.513 2.85 3.591 3.003V15h1.043v-1.216c2.27-.179 3.678-1.438 3.678-3.3 0-1.59-.947-2.51-2.956-3.028l-.722-.187V3.467c1.122.11 1.879.714 2.07 1.616h1.47c-.166-1.6-1.54-2.748-3.54-2.875V1H7.591v1.233c-1.939.23-3.27 1.472-3.27 3.156 0 1.454.966 2.483 2.661 2.917l.61.162v4.031c-1.149-.17-1.94-.8-2.131-1.718H4zm3.391-3.836c-1.043-.263-1.6-.825-1.6-1.616 0-.944.704-1.641 1.8-1.828v3.495l-.2-.05zm1.591 1.872c1.287.323 1.852.859 1.852 1.769 0 1.097-.826 1.828-2.2 1.939V8.73l.348.086z"/></svg><span>2000.00</span></p>
+        </div>
+    </div>
 </section>
