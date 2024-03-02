4d7dd9deb62b17fa0722303955ada9a06ae343ce
Naio200000 <90395089+Naio200000@users.noreply.github.com>
2024-03-02 01:35:54 -0300

    feat: add eliminar item carrito

--- admin/acciones/item-acc.php
+++ admin/acciones/item-acc.php
@@ -3,9 +3,18 @@
     require_once "../../libraries/autoloader.php";
 
     $id = $_GET['id'] ?? false;
+    $del = $_GET['del'] ?? false;
     $cantidad = $_GET['cantidad'];
 
-    if ($id) {
-        (new Carrito)->agregar_producto($id, $cantidad);
-        header('location: ../../index.php?view=carrito');
+    if ($del) {
+        if ($id) {
+            (new Carrito)->eliminarProductoId($id, $cantidad);
+            header('location: ../../index.php?view=carrito');
+        }
+    } else {
+        if ($id) {
+            (new Carrito)->agregarProductoId($id, $cantidad);
+            header('location: ../../index.php?view=carrito');
+        }
+        
     }
--- classes/Carrito.php
+++ classes/Carrito.php
@@ -8,16 +8,43 @@ class Carrito {
      * @param int $idProducto Id del producto a agregar
      * @param int $cantidad Cantidad de productos
      */
-    public function agregar_producto (int $idProducto, int $cantidad) {
+    public function agregarProductoId (int $idProducto, int $cantidad) {
         
         $datosProducto = (new Producto)->productoID($idProducto);
 
-            echo "<pre>";
-            print_r($datosProducto);
-            echo "</pre>";
+            if($datosProducto) {
+                $_SESSION['carrito'][$idProducto] = [
+                    'nombre' => $datosProducto->getNombre(),
+                    'imagen' => $datosProducto->getImagen()[0],
+                    'precio' => $datosProducto->formatearPrecio(),
+                    'cantidad' => $cantidad
+                ];
+            }
+    }
+
+    /**
+     * Elimina un item de la lista de compras
+     * @param int $idProducto el ID del producto a eliminar
+     */
+    public function eliminarProductoId (int $idProducto) { 
+
+        if (isset($_SESSION['carrito'][$idProducto])) {
+            unset($_SESSION['carrito'][$idProducto]);
+        }
 
     }
 
+    /**
+     * Devuelve el contenido del carrito que se encuentre guardado en la SESSION
+     * #@return array El contenido del carrito en la SESSIOn o un array vacio.
+     */
+    public function get_carrito() :array {
+
+        if (!empty($_SESSION['carrito'])) {
+            return $_SESSION['carrito'];
+        }
+        return [];
+    }
 
 
 }
